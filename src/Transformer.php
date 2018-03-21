bae44b95bec2b129a59c5394c8f5d37f18a0dd1d
Fenz <uukooo@126.com>
2018-03-21 18:06:28 +0800

    Add class Transformer
    use support\TStartWithMetaArray;
    ::transform_() Unify data with a Meta object;
    ::transformItem_() Unify an item with a MetaItem object;
    
    ->__construct() Construct with a Meta object;
    ->transform() Unify data with inside meta;

---
+++ src/Transformer.php
@@ -0,0 +1,15 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Fenzland\DataParser;
+
+////////////////////////////////////////////////////////////////
+
+class Transformer
+{
+	const PARSE_METHOD= 'transform';
+
+	use Transformer\__static;
+	use Transformer\__object;
+}
